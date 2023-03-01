cab349fd796a39da50b223dec49b131994099f1c
JLZ22 <johnzeng@youmewho.com>
2023-02-28 21:02:33 -0500

    threading is broken beyond belief

--- brc_arm_motor_driver/brc_arm_motor_driver/odrive_joint.py
+++ brc_arm_motor_driver/brc_arm_motor_driver/odrive_joint.py
@@ -1,6 +1,6 @@
 import time
-import logging
 import threading
+from odrive.enums import *
 import odrive
 
 class OdriveJoint:
@@ -8,28 +8,29 @@ class OdriveJoint:
     self,
     name,
     odr: odrive,
-    trajectory_limits,
   ):
     self.name = name
     self.odr = odr
     self.axis = getattr(odr, "axis1")
-    self.configure_trajectory_control(trajectory_limits[0], 
-                                      trajectory_limits[1], 
-                                      trajectory_limits[2], 
-                                      trajectory_limits[3],
-                                      trajectory_limits[4])
     self.is_homed = False
     self.desired_pos = 0
+    self.is_alive = True
     # self.odr.controller.config.control_mode = VELOCITY_CONTROL
-    self.thread = threading.Thread(target=self.thread_pos_controller, args=(1,), daemon=True)
-    self.thread.start()
+    self.start_thread()
     
+  def start_thread(self):
+    threading.Thread(target=self.threaded_pos_controller, daemon=True).start()
+  
   def threaded_pos_controller(self): 
-    while True:
+    print("running thread")
+    while self.is_alive:
       k = 1 # tune PID value
       self.axis.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
       self.axis.controller.input_vel = k * (self.desired_pos - self.axis.encoder.pos_estimate)
-      time.sleep(0.01)
+      # print(self.axis.encoder.vel_estimate)
+      print(self.axis.encoder.pos_estimate)
+      print("input vel: ", self.axis.controller.input_vel)
+      time.sleep(0.5)
 
   def go_to_position(self, position):
     try: 
@@ -41,22 +42,6 @@ class OdriveJoint:
       self.stop()
       print(f"Joint {self.name}: \t function: go_to_position | error: {e}")
       print(e)
-    
-  def configure_trajectory_control(
-    self, bandwidth, vel_limit, accel_limit, decel_limit, inertia):
-    self.logger.info("configuring axis")
-    try:
-      self.axis.controller.config.input_filter_bandwidth = bandwidth
-      self.axis.controller.config.input_mode = InputMode.TRAP_TRAJ
-      self.odr.controller.config.control_mode = ControlMode.POSITION_CONTROL
-      self.axis.trap_traj.config.vel_limit = vel_limit
-      self.axis.trap_traj.config.accel_limit = accel_limit
-      self.axis.trap_traj.config.decel_limit = decel_limit
-      self.axis.controller.config.inertia = inertia
-    except Exception as e: 
-      self.stop()
-      self.logger.warn(f"Joint {self.name}: \t function: configure_trajectory_control | error: {e}")
-      self.logger.warn(e)
   
   def home_joint(self):
     start_time = time.monotonic()
@@ -79,8 +64,8 @@ class OdriveJoint:
       self.axis.max_endstop.config.enabled = True
       self.odr.config.gpio2_mode = GPIO_MODE_DIGITAL_PULL_UP
       self.odr.save_configuration()
-      while self.axis.current_state != HOMING:
-        self.axis.requested_state = HOMING
+      while self.axis.current_state != AXIS_STATE_HOMING:
+        self.axis.requested_state = AXIS_STATE_HOMING
         if start_time - time.monotonic() > timeout:
           print(
             f"Homing joint {self.name} failed, took longer than {timeout}s"
@@ -98,28 +83,37 @@ class OdriveJoint:
     
   def stop(self): 
     self.axis.requested_state = AXIS_STATE_IDLE
-    self.odr.controller.config.control_mode = VELOCITY_CONTROL
     self.axis.controller.input_vel = 0
     print(f"Joint {self.name}:\t command sent, stop")
     
 def main():
+  print('Starting...')
   while True:
     odr = odrive.find_any()
     if odr != None:
       break
     time.sleep(0.5)
-  odr_joint = OdriveJoint("odrv_arm", odr, [1, 1, 1, 1, 1])
-  while True:
-    x = int(input("\nEnter a function:\n (0) quit\n (1) go_to_position(position)\n (2) home_joint()\n (3) stop()\n"))
-    if x == 1:
-      y = float(input("Enter a position\n"))
-      odr_joint.go_to_position(y)
-    elif x == 2:
-      odr_joint.home_joint()
-    elif x == 3: 
-      odr_joint.stop()
-    elif x == 0:
-      break
+  try: 
+    odr_joint = OdriveJoint("odrv_arm", odr)
+    while True:
+      x = int(input("\nEnter a function:\n (0) quit\n (1) go_to_position(position)\n (2) home_joint()\n (3) stop()\n"))
+      if x == 1:
+        y = float(input("Enter a position\n"))
+        odr_joint.go_to_position(y)
+      elif x == 2:
+        odr_joint.home_joint()
+      elif x == 3: 
+        odr_joint.stop()
+      elif x == 0:
+        break
+    print('Goodbye!')
+  except KeyboardInterrupt as e: 
+    odr_joint.is_alive = False 
+    # odr_joint.stop()
+    print("Main had a keyboard interupt: {e}") 
+  except Exception as e: 
+    print("Main threw an exception: {e}")
+    
 
 if __name__ == "__main__":
     main()
---
+++ brc_arm_motor_driver/brc_arm_motor_driver/stop_odrive.py
@@ -0,0 +1,7 @@
+import odrive
+from odrive.enums import *
+
+odr = odrive.find_any()
+axis = getattr(odr, "axis1")
+axis.requested_state = AXIS_STATE_IDLE
+axis.controller.input_vel = 0
